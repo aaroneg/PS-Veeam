4405bd0a7fa3d273c543d8ee301964929bca318f
Aaron <aaroneg@users.noreply.github.com>
2018-12-19 23:03:14 -0600

    Update initialize-veeam.ps1
    comments.

--- initialize-veeam.ps1
+++ initialize-veeam.ps1
@@ -2,9 +2,10 @@
 PARAM(
         [Parameter(Mandatory=$True)][string]$VeeamServer
 )
+# import the veeam snapin.
 Add-PSSnapin VeeamPSSnapin
+# if we aren't already connected, connect
 if (!(Get-VBRServerSession)) {
     Write-Host "No existing session found, connecting to Veeam" -ForegroundColor Yellow
     Connect-VBRServer -server $VeeamServer
 }
-
