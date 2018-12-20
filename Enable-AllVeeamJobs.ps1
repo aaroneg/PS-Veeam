2939985d1a7358a6c2fddeda6d9b541fe8c3a289
Aaron <aaroneg@users.noreply.github.com>
2018-12-19 23:04:09 -0600

    Update Enable-AllVeeamJobs.ps1

--- Enable-AllVeeamJobs.ps1
+++ Enable-AllVeeamJobs.ps1
@@ -1,3 +1,5 @@
 ﻿. $PSScriptRoot\initialize-veeam.ps1
+# ignore dev and test machines
 $VeeamJobs=get-vbrjob|Where {$_.Name -notlike "DEV*" -and $_.Name -notlike "TEST*"}|Sort -Property Name
+# enable the job.
 $VeeamJobs|Enable-VBRJob
